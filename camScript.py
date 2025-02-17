4db54888e9fa54fdca763f1b528ad5854673662f
microplants <microplants@raspberrypi>
2025-02-17 16:37:27 +0100

    changed script to take images, correct parameters still need to be set

--- camScript.py
+++ camScript.py
@@ -1,10 +1,10 @@
-from picamera import PiCamera
+from picamzero import Camera
 from time import sleep
 
 #script for taking photos, not finished
 
-cam = PiCamera()
+cam = Camera()
 
 cam.start_preview()
-cam.capture_sequence()
+cam.capture_sequence(f"Pictures/sequence.jpg", num_images=3, interval=2)
 cam.stop_preview()
